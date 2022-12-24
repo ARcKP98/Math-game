dfa3d5d08d65ff03967665c3560f36919a790969
ARcKP98 <pkartik4386@outlook.com>
2022-12-24 15:18:30 +0000

    Add multiplication code.

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,6 +29,8 @@ function runGame(gameType){
 
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
+    } else if(gameType === "multiply") {
+        displayMultiplicationQuestion(num1, num2);
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
@@ -66,6 +68,8 @@ function calculateCorrectAnswer(){
 
     if(operator === "+") {
         return [operand1 + operand2, "addition"];
+    } else  if(operator === "x") {
+        return [operand1 * operand2, "multiply"];
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -79,6 +83,10 @@ function incrementScore(){
     let oldScore = parseInt(document.getElementById("score").innerText);
     document.getElementById("score").innerText = ++oldScore;
 }
+
+/**
+ * Gets the current tally of incorrect answers from the DOM and increments it by 1
+ */
 function incrementWrongAnswer(){
     let oldScore = parseInt(document.getElementById("incorrect").innerText);
     document.getElementById("incorrect").innerText = ++oldScore;
@@ -91,8 +99,10 @@ function displayAdditionQuestion(operand1, operand2) {
 function displaySubtractionQuestion() {
     
 }
-function displayMultiplicationQuestion() {
-    
+function displayMultiplicationQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "x";
 }
 function displayDivisionQuestion() {
     
