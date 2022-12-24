a3c919cc59cd221def09c160087bbe28bae8c510
ARcKP98 <pkartik4386@outlook.com>
2022-12-24 14:31:59 +0000

    Add code to increment the correct and incorrect score.

--- assets/js/script.js
+++ assets/js/script.js
@@ -46,8 +46,10 @@ function checkAnswer() {
 
     if (isCorrect) {
         alert("Hey! You got it right! :D ");
+        incrementScore();
     } else {
         alert(`That was incorrect. You answered ${userAnswer} but the correct answer was ${calculatedAnswer[0]}!`)
+        incrementWrongAnswer();
     }
 
     runGame(calculatedAnswer[1]);
@@ -69,11 +71,17 @@ function calculateCorrectAnswer(){
         throw `Unimplemented operator ${operator}. Aborting!`;
     }
 }
-function incrementScore(){
 
+/**
+ * Gets the current score from the DOM and increments it by 1
+ */
+function incrementScore(){
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
 }
 function incrementWrongAnswer(){
-
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
 }
 function displayAdditionQuestion(operand1, operand2) {
     document.getElementById('operand1').textContent = operand1;
